b0990c9ebd169ad4ef96f674cb4d19eb36acfffa
Edgardo Marchi <edgardo.marchi@gmail.com>
2019-08-05 17:49:01 -0300

    Changed example_python to ints

--- example_python/fpgaInterface.py
+++ example_python/fpgaInterface.py
@@ -5,7 +5,7 @@ s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.10', 7))
 
 
-data = np.random.randn(100)
+data = np.random.randint(0,100,10)
 print(data)
 
 s.send(data.tobytes())
@@ -13,5 +13,5 @@ print('Bytes sent: {0!s}'.format(len(data.tobytes())))
 recvData = s.recv(len(data.tobytes()))
 print('Bytes received: {0!s}'.format(len(recvData)))
 
-rxData = np.frombuffer(recvData)
+rxData = np.frombuffer(recvData, dtype=int)
 print(rxData)
